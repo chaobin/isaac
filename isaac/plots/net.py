d9d3cf54b3b4bcad40f82c94a70d98c1679ce992
Chaobin <cbtchn@gmail.com>
2016-06-12 01:34:37 +0800

    store rendered results for nbconvert

--- isaac/plots/net.py
+++ isaac/plots/net.py
@@ -1,13 +1,18 @@
+import os
 from itertools import product
+from contextlib import contextmanager
 
 import graphviz as gv
 from isaac.plots import dot
 
 
 __all__ = [
+    'folder',
     'forward_net'
 ]
 
+# used to render the svg
+__NAME = None
 
 # http://www.graphviz.org/doc/info/attrs.html
 nn_styling = {
@@ -45,6 +50,27 @@ nn_styling = {
 }
 
 
+@contextmanager
+def folder(root, ext='png'):
+    global __NAME
+    if not os.path.exists(root): os.mkdir(root)
+    cursor = os.path.join(root, 'next')
+    name = None
+    if not os.path.exists(cursor):
+        with open(cursor, 'w') as f:
+            name = str(0)
+            f.write(name)
+    else:
+        with open(cursor) as f:
+            name = f.read()
+    __NAME = os.path.join(root, os.path.extsep.join((name, ext)))
+    yield
+    name = int(name)
+    name += 1
+    with open(cursor, 'w') as f:
+        f.write(str(name))
+    __NAME = None
+
 def forward_net(layering=(),
     labels=('input', 'hidden', 'output'), reversed=False):
     label_i, label_h, label_o = labels
@@ -79,4 +105,6 @@ def forward_net(layering=(),
     if reversed:
         root.edge_attr.update({'dir': 'both', 'arrowhead': 'none', 'arrowtail': 'open'})
     # root.body.extend(["%s=%s" % (k, v) for (k, v) in nn_styling['graph'].items()])
+    # Stores this so that the nvconvert will have it
+    root.render(__NAME, view=True)
     return root
